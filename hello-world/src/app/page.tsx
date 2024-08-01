9fb147109502bc114762b1a10c7a01aa8eab45ce
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2024-08-02 01:08:23 +0530

    active links and Link

--- hello-world/src/app/page.tsx
+++ hello-world/src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Welcome Home!</div>;
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <Link href="/dashboard">Dashboard</Link>
+    </>
+  );
 }
