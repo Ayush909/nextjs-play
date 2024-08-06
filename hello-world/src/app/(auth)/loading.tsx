36641790de086bbb08cd1168cb50d4cda164ea42
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2024-08-07 00:47:55 +0530

    loading

---
+++ hello-world/src/app/(auth)/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LoadingUI = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quibusdam
+      dolore illo consequuntur error explicabo fugiat debitis saepe harum
+      veniam, perferendis qui earum odit repudiandae architecto corrupti velit
+      eaque mollitia. Loading...
+    </div>
+  );
+};
+
+export default LoadingUI;
